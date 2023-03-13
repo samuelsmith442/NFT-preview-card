0c441ec2e70572de261090f10270dd1d14751b3e
Samuel <samuelsmith442@gmail.com>
2023-03-13 02:00:04 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        softBlue: 'hsl(215, 51%, 70%)',
+        cyan: 'hsl(178, 100%, 50%)',
+        veryDarkblue1: 'hsl(217, 54%, 11%)',
+        veryDarkblue2: 'hsl(216, 50%, 16%)',
+        veryDarkblue3: 'hsl(215, 32%, 27%)',
+        white: 'hsl(0, 0% 100%)',
+      },
+      fontFamily: {
+        sans: ['Outfit', 'sans-serif '],
+      }
+    },
+  },
+  plugins: [],
+}
